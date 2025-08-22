0bdbe1a2a0ee07dfd66298f96cb60ef025b47c1a
woddu <fwoddu@yahoo.com>
2025-08-22 21:24:25 +0800

    created a separate interface for inventory events and class for inventory state

---
+++ app/src/main/java/com/example/firebaseapptest/ui/event/InventoryEvent.kt
@@ -0,0 +1,36 @@
+package com.example.firebaseapptest.ui.event
+
+import com.example.firebaseapptest.ui.view.AppEvent
+
+
+sealed interface InventoryEvent {
+
+    object OnScanButtonClickedFromInventory : InventoryEvent
+
+    object OnInventoryNextPage: InventoryEvent
+    object OnInventoryPreviousPage: InventoryEvent
+    object OnInventoryFirstPage: InventoryEvent
+    object OnInventoryLastPage: InventoryEvent
+    data class OnInventoryPageChanged(val page: Int): InventoryEvent
+
+    object OnInventoryAddButtonClicked: InventoryEvent
+    object OnInventoryAddCanceled: InventoryEvent
+    data class OnInventorySetItemName(val name: String): InventoryEvent
+    data class OnInventorySetItemPrice(val price: String): InventoryEvent
+    data class OnInventorySetItemQuantity(val quantity: String): InventoryEvent
+    data class OnInventorySetItemDescription(val description: String): InventoryEvent
+    data class OnInventorySetItemColor(val color: String): InventoryEvent
+    data class OnInventorySetItemCode(val code: String): InventoryEvent
+    data class OnInventorySetItemSize(val size: String): InventoryEvent
+    data class OnInventorySetItemSold(val sold: String): InventoryEvent
+    object OnInventoryAddConfirmed: InventoryEvent
+
+    data class OnInventoryItemDetails(val code: Long): InventoryEvent
+
+    object OnInventoryEditConfirmed: InventoryEvent
+    object OnInventoryDeleteWarning: InventoryEvent
+    data class OnInventoryDeleteConfirmed(val code: Long): InventoryEvent
+    object OnInventoryDeleteCanceled: InventoryEvent
+
+
+}
---
+++ app/src/main/java/com/example/firebaseapptest/ui/state/InventoryState.kt
@@ -0,0 +1,23 @@
+package com.example.firebaseapptest.ui.state
+
+import com.example.firebaseapptest.data.local.entity.ItemForSale
+
+data class InventoryState(
+    val items : List<ItemForSale> = emptyList(),
+    val scannedText: String = "",
+    val showFormDialog: Boolean = false,
+
+    val currentPage: Int = 1,
+    val lastPage: Int = 1,
+
+    val itemName: String = "",
+    val itemPrice: String = "",
+    val itemQuantity: String = "",
+    val itemDescription: String = "",
+    val itemColor: String = "",
+    val itemCode: String = "",
+    val itemSize: String = "",
+    val itemSold: String = "",
+
+    val deleteConfirmationDialog: Boolean = false,
+)
