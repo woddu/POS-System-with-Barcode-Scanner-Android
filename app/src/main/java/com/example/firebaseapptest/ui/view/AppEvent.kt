b3556a2b06a8e0f625fa911c7d8416df7e83e878
woddu <fwoddu@yahoo.com>
2025-08-23 21:13:24 +0800

    remove the events for inventory

--- app/src/main/java/com/example/firebaseapptest/ui/view/AppEvent.kt
+++ app/src/main/java/com/example/firebaseapptest/ui/view/AppEvent.kt
@@ -1,21 +1,10 @@
 package com.example.firebaseapptest.ui.view
 
 sealed interface AppEvent {
-    object OnScanButtonClicked : AppEvent
+    object OnScanButtonClickedFromHome : AppEvent
     data class OnBarcodeScanned(val text: String) : AppEvent
     object OnScannerConsumed : AppEvent
     object OnScanComplete: AppEvent
 
-    object OnInventoryAddButtonClicked: AppEvent
-    object OnInventoryAddCanceled: AppEvent
-    data class OnInventorySetItemName(val name: String): AppEvent
-    data class OnInventorySetItemPrice(val price: String): AppEvent
-    data class OnInventorySetItemQuantity(val quantity: String): AppEvent
-    data class OnInventorySetItemDescription(val description: String): AppEvent
-    data class OnInventorySetItemColor(val color: String): AppEvent
-    data class OnInventorySetItemCode(val code: String): AppEvent
-    data class OnInventorySetItemSize(val size: String): AppEvent
-    data class OnInventorySetItemSold(val sold: String): AppEvent
-    object OnInventoryAddConfirmed: AppEvent
 
 }
