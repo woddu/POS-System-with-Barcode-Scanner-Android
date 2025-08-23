c51c5b9b243e0f27dac47f51d94b3d7ed4088b51
woddu <fwoddu@yahoo.com>
2025-08-23 21:09:56 +0800

    added a getCount function for pagination

--- app/src/main/java/com/example/firebaseapptest/data/local/dao/ItemDao.kt
+++ app/src/main/java/com/example/firebaseapptest/data/local/dao/ItemDao.kt
@@ -5,23 +5,30 @@ import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
 import com.example.firebaseapptest.data.local.entity.Item
+import com.example.firebaseapptest.data.local.entity.ItemForSale
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ItemDao {
 
     @Upsert
-    suspend fun addItem(item: Item)
+    suspend fun upsertItem(item: Item): Long
 
     @Delete
     suspend fun deleteItem(item: Item)
 
+    @Query("SELECT COUNT(*) FROM items")
+    suspend fun getCount(): Int
+
     @Query("SELECT code, name, price FROM items ORDER BY name")
-    fun getItems(): Flow<List<Item>>
+    fun getItems(): Flow<List<ItemForSale>>
+
+    @Query("SELECT code, name, price FROM items ORDER BY name LIMIT :limit OFFSET :offset")
+    fun getItemsPaginated(limit: Int, offset: Int): Flow<List<ItemForSale>>
 
     @Query("SELECT * FROM items WHERE code = :code")
-    fun getItemByCode(code: Long): Item
+    suspend fun getItemByCode(code: Long): Item
 
     @Query("SELECT code, name, price FROM items WHERE code = :code")
-    fun getItemByCodeForSale(code: Long): Item
+    suspend fun getItemByCodeForSale(code: Long): ItemForSale
 }
