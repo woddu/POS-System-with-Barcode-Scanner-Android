d829f35d6c116e883e8bdb41b3e39312729dd34e
woddu <fwoddu@yahoo.com>
2025-08-23 21:11:05 +0800

    fixed the return type of insert and added a Transaction of getting sale with items

--- app/src/main/java/com/example/firebaseapptest/data/local/dao/SaleDao.kt
+++ app/src/main/java/com/example/firebaseapptest/data/local/dao/SaleDao.kt
@@ -2,16 +2,18 @@ package com.example.firebaseapptest.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Delete
+import androidx.room.Insert
 import androidx.room.Query
-import androidx.room.Upsert
+import androidx.room.Transaction
 import com.example.firebaseapptest.data.local.entity.Sale
+import com.example.firebaseapptest.data.local.relations.SaleWithItems
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SaleDao {
 
-    @Upsert
-    suspend fun addSale(sale: Sale)
+    @Insert
+    suspend fun addSale(sale: Sale): Long
 
     @Delete
     suspend fun deleteSale(sale: Sale)
@@ -25,6 +27,13 @@ interface SaleDao {
         ORDER BY date DESC
     """)
     fun getTodaySales(startOfDay: Long, endOfDay: Long): Flow<List<Sale>>
+
+    @Query("SELECT * FROM sales WHERE id = :id")
+    suspend fun getSale(id: Int): Sale
+
+    @Transaction
+    @Query("SELECT * FROM sales WHERE id = :id")
+    suspend fun getSaleWithItems(id: Int): SaleWithItems
 }
 
 /*
