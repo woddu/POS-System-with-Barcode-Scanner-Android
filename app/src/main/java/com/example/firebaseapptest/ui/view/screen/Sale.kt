9e2fa35e5ee51c1fef56a0f9abe378a659dec360
woddu <fwoddu@yahoo.com>
2025-08-30 17:14:38 +0800

    working on sales view

--- app/src/main/java/com/example/firebaseapptest/ui/view/screen/Sale.kt
+++ app/src/main/java/com/example/firebaseapptest/ui/view/screen/Sale.kt
@@ -2,26 +2,107 @@ package com.example.firebaseapptest.ui.view.screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.firebaseapptest.ui.view.AppState
+import com.example.firebaseapptest.ui.view.screen.components.SimpleCard
+
 
 @Composable
-fun Sale(){
+fun Sale(
+    state: AppState
+) {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier.fillMaxSize()
     ) {
-        // Sale screen content
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(.85f)
+                .padding(horizontal = 8.dp)
+        ) {
+            items(state.items, key = { item -> item.code }) { item ->
+                SimpleCard(
+                    roundedCornerShape = 12.dp,
+                    elevation = 8.dp
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth()
+                            .padding(top = 8.dp, bottom = 8.dp, start = 12.dp)
+                    ) {
+                        Text(
+                            text = item.name,
+                            style = MaterialTheme.typography.labelLarge,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 24.sp,
+                            modifier = Modifier.fillMaxWidth(.5f)
+                        )
+                        Row(
+                            horizontalArrangement = Arrangement.End,
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(
+                                text = "₱ " + item.price.toString(),
+                                style = MaterialTheme.typography.labelLarge,
+                                fontSize = 24.sp,
+                            )
+                            IconButton(onClick = {
 
+                            }) {
+                                Icon(
+                                    imageVector = Icons.Default.MoreVert,
+                                    contentDescription = "details"
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            IconButton(
+                onClick = {  },
+                enabled = state.currentPage > 1
+            ) {
+                Icon(imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft, contentDescription = "Previous Page")
+            }
+            Text(state.currentPage.toString() + "/" + state.lastPage.toString())
+            IconButton(
+                onClick = {  },
+                enabled = state.currentPage < state.lastPage
+            ) {
+                Icon(imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight, contentDescription = "Next Page")
+            }
+        }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun SalePreview(){
-    Sale()
 }
