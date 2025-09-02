2966e34a237277e59732655dc7d0ecd1bbf5c7ed
woddu <fwoddu@yahoo.com>
2025-09-03 00:10:13 +0800

    created a helper model for sale item with item name

---
+++ app/src/main/java/com/example/firebaseapptest/data/local/entity/helpermodels/SaleItemNameOnly.kt
@@ -0,0 +1,10 @@
+package com.example.firebaseapptest.data.local.entity.helpermodels
+
+data class SaleItemNameOnly(
+    val saleItemId: Int,
+    val saleId: Int,
+    val itemCode: Long,
+    val quantity: Int,
+    val price: Double,
+    val itemName: String
+)
