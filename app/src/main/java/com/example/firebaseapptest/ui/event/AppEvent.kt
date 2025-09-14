4764a0e795b5a36e09e33b6496a63e75ed9f5919
woddu <fwoddu@yahoo.com>
2025-09-14 20:47:04 +0800

    added events for OnImageCropped, OnTempFileCreated, OnImageUriChanged, OnChoosePaymentMethod and changes to OnAddSale

--- app/src/main/java/com/example/firebaseapptest/ui/event/AppEvent.kt
+++ app/src/main/java/com/example/firebaseapptest/ui/event/AppEvent.kt
@@ -1,13 +1,24 @@
 package com.example.firebaseapptest.ui.event
 
+import android.content.Context
+import android.net.Uri
 import com.example.firebaseapptest.ui.view.SalesFilter
+import java.io.File
 
 sealed interface AppEvent {
     object OnScanButtonClickedFromHome : AppEvent
     data class OnBarcodeScanned(val text: String) : AppEvent
     object OnScannerConsumed : AppEvent
 
-    object OnAddSale: AppEvent
+    data class OnImageCropped(val uri: Uri) : AppEvent
+
+    data class OnTempImageFileCreated(val photoFile: File) : AppEvent
+    data class OnImageUriChanged(val uri: Uri?) : AppEvent
+
+    data class OnChoosePaymentMethod(val method: String): AppEvent
+
+    data class OnAddSale(val context: Context): AppEvent
+
     object OnCancelSale: AppEvent
 
     data class OnFilterSales(val salesFilter: SalesFilter, val betweenDates: Pair<Long, Long>?): AppEvent
