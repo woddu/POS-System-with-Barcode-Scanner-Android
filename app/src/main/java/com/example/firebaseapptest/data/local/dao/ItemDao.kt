7b0cd925845c035f8df256fd9479ca76d60ef06d
woddu <fwoddu@yahoo.com>
2025-10-03 19:23:39 +0800

    added columns: discount and isDiscountPercentage in query returns

--- app/src/main/java/com/example/firebaseapptest/data/local/dao/ItemDao.kt
+++ app/src/main/java/com/example/firebaseapptest/data/local/dao/ItemDao.kt
@@ -23,18 +23,18 @@ interface ItemDao {
     @Query("SELECT COUNT(*) FROM items")
     suspend fun getCount(): Int
 
-    @Query("SELECT code, name, price FROM items ORDER BY name")
+    @Query("SELECT code, name, price, discount, isDiscountPercentage FROM items ORDER BY name")
     fun getItems(): Flow<List<ItemForSale>>
 
-    @Query("SELECT code, name, price FROM items ORDER BY name LIMIT :limit OFFSET :offset")
+    @Query("SELECT code, name, price, discount, isDiscountPercentage FROM items ORDER BY name LIMIT :limit OFFSET :offset")
     fun getItemsPaginated(limit: Int, offset: Int): Flow<List<ItemForSale>>
 
-    @Query("SELECT code, name, price FROM items WHERE name LIKE '%' || :searchQuery || '%' ORDER BY name LIMIT :limit OFFSET :offset ")
+    @Query("SELECT code, name, price, discount, isDiscountPercentage FROM items WHERE name LIKE '%' || :searchQuery || '%' ORDER BY name LIMIT :limit OFFSET :offset ")
     fun getSearchQueryPaginated(searchQuery: String, limit: Int, offset: Int): Flow<List<ItemForSale>>
 
     @Query("SELECT * FROM items WHERE code = :code")
     suspend fun getItemByCode(code: Long): Item?
 
-    @Query("SELECT code, name, price FROM items WHERE code = :code")
+    @Query("SELECT code, name, price, discount, isDiscountPercentage FROM items WHERE code = :code")
     suspend fun getItemByCodeForSale(code: Long): ItemForSale?
 }
