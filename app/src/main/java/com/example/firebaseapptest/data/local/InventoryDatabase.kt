58531fd975e03bb8bee96a7c1aeb772dd699c2fc
woddu <fwoddu@yahoo.com>
2025-10-18 22:27:41 +0800

    updated database class

--- app/src/main/java/com/example/firebaseapptest/data/local/InventoryDatabase.kt
+++ app/src/main/java/com/example/firebaseapptest/data/local/InventoryDatabase.kt
@@ -5,6 +5,7 @@ import androidx.room.RoomDatabase
 import com.example.firebaseapptest.data.local.dao.ItemDao
 import com.example.firebaseapptest.data.local.dao.SaleDao
 import com.example.firebaseapptest.data.local.dao.SaleItemDao
+import com.example.firebaseapptest.data.local.dao.UserDao
 import com.example.firebaseapptest.data.local.entity.Item
 import com.example.firebaseapptest.data.local.entity.Sale
 import com.example.firebaseapptest.data.local.entity.SaleItem
@@ -17,4 +18,5 @@ abstract class InventoryDatabase : RoomDatabase() {
     abstract fun itemDao(): ItemDao
     abstract fun saleDao(): SaleDao
     abstract fun saleItemDao(): SaleItemDao
+    abstract fun userDao(): UserDao
 }
