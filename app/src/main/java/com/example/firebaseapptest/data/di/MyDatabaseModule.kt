236d2e6016df52261221cd9db05bbd80abc4d021
woddu <fwoddu@yahoo.com>
2025-10-18 22:28:25 +0800

    updated hilt module and added seed data

--- app/src/main/java/com/example/firebaseapptest/data/di/MyDatabaseModule.kt
+++ app/src/main/java/com/example/firebaseapptest/data/di/MyDatabaseModule.kt
@@ -2,15 +2,19 @@ package com.example.firebaseapptest.data.di
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.firebaseapptest.data.local.InventoryDatabase
 import com.example.firebaseapptest.data.local.dao.ItemDao
 import com.example.firebaseapptest.data.local.dao.SaleDao
 import com.example.firebaseapptest.data.local.dao.SaleItemDao
+import com.example.firebaseapptest.data.local.dao.UserDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import java.security.MessageDigest
 import javax.inject.Singleton
 
 @Module
@@ -21,6 +25,16 @@ object MyDatabaseModule {
     @Singleton
     fun provideDatabase(@ApplicationContext context: Context): InventoryDatabase {
         return Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
+            .addCallback(object: RoomDatabase.Callback() {
+                override fun onCreate(db: SupportSQLiteDatabase) {
+                    super.onCreate(db)
+                    val bytes = MessageDigest.getInstance("SHA-256")
+                        .digest("@PoSE12025".toByteArray())
+                    val hashedPassword = bytes.joinToString("") { "%02x".format(it) }
+                    // Insert seed data directly with SQL
+                    db.execSQL("INSERT OR IGNORE INTO User (id, email, username, password, isOwner) VALUES (0, 'employee1@pos.com', 'employee1', '$hashedPassword', 0)")
+                }
+            })
             .fallbackToDestructiveMigration(false)
             .build()
     }
@@ -33,4 +47,7 @@ object MyDatabaseModule {
 
     @Provides
     fun provideSaleItemDao(db: InventoryDatabase): SaleItemDao = db.saleItemDao()
+
+    @Provides
+    fun provideUserDao(db: InventoryDatabase): UserDao = db.userDao()
 }
