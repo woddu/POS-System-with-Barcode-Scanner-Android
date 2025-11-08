663c4a715795759fd82f7b4e2c590f0bfbe44303
woddu <fwoddu@yahoo.com>
2025-11-08 23:04:15 +0800

    added login page

---
+++ app/src/main/java/com/example/firebaseapptest/ui/view/screen/Login.kt
@@ -0,0 +1,13 @@
+package com.example.firebaseapptest.ui.view.screen
+
+import androidx.compose.runtime.Composable
+import com.example.firebaseapptest.ui.event.AppEvent
+import com.example.firebaseapptest.ui.state.AppState
+
+@Composable
+fun Login(
+    state: AppState,
+    onEvent: (AppEvent) -> Unit
+) {
+
+}
