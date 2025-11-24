dfa8a02aa6a3ac665a22e8d8a55bec489f4797b3
woddu <fwoddu@yahoo.com>
2025-11-24 15:12:49 +0800

    late hilt and hilt work dependencies commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.hilt.common)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -76,6 +77,8 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     implementation(libs.androidx.navigation.compose)
+
+    implementation("androidx.work:work-runtime-ktx:2.10.5")
 
     implementation(libs.zxing.android.embedded)
     implementation(libs.core)
@@ -92,12 +95,15 @@ dependencies {
     implementation(libs.androidx.hilt.lifecycle.viewmodel.compose)
     ksp(libs.androidx.hilt.compiler)
 
+    implementation("androidx.hilt:hilt-work:1.2.0")
+
     implementation(libs.androidx.hilt.navigation.compose)
 
 
     implementation("com.github.yalantis:ucrop:2.2.8")
 
     implementation("io.coil-kt:coil-compose:2.4.0")
+
     implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
